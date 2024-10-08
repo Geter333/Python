b0ee2e407d6ec43b7a97edaed5889563ef0c40a4
Geter333 <bogdan2004ruban@gmail.com>
2024-10-08 21:30:13 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,3 +0,0 @@
-sentence = "Це приклад речення з різними словами, це речення демонстраційне."
-words = set(sentence.lower().replace(",", "").replace(".", "").split())
-print("Унікальні слова в реченні:", words)
